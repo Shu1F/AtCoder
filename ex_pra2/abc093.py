2b6786702c373522503998ce8b7e2cbaccd1f888
Shuichi Fujiike <fs2031@icloud.com>
2026-01-11 12:08:28 +0900

    fix abc093

--- ex_pra2/abc093.py
+++ ex_pra2/abc093.py
@@ -1,5 +1,12 @@
 A, B, K = map(int, input().split())
 
-for i in range(A, B + 1):
-    if (A <= i and i <= A + K - 1) or (B - K + 1 <= i and i <= B):
-        print(i)
+s = set()
+
+for x in range(A, min(B, A + K - 1) + 1):
+    s.add(x)
+
+for x in range(max(A< B - K + 1), B + 1):
+    s.add(x)
+
+for x in sorted(s):
+    print(x)
