00125485eb0c109e38797c61e2d95cb8f1b24972
Shuichi Fujiike <fs2031@icloud.com>
2026-02-11 11:40:19 +0900

    update

--- section1/abc033.py
+++ section1/abc033.py
@@ -5,15 +5,15 @@ if H == 1 or W == 1:
         print((H * W) // 2)
     else:
         print((H * W) // 2 + 1)
-elif H >= 2 and W >= 2:
+else:
     if H % 2 == 0:
         height = H // 2
     else:
-        height = H // 2 + 1
+        height = (H + 1) // 2
 
     if W % 2 == 0:
         width = W // 2
     else:
-        width = W // 2 + 1
+        width = (W + 1) // 2
 
     print(height * width)
