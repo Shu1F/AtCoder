e02fa6a2f7ae4da5689bfc45d3d80e0315622df2
Shuichi Fujiike <fs2031@icloud.com>
2026-02-13 19:35:11 +0900

    fin abc162b

--- setion2/abc162b.py
+++ setion2/abc162b.py
@@ -1,10 +1,19 @@
-N = int(input())
-sum = 0
-while N != 0:
-    if N % 3 != 0 and N % 5 != 0:
-        sum += N
-        N -= 1
-    else:
-        N -= 1
+# N = int(input())
+# sum = 0
+# while N != 0:
+#     if N % 3 != 0 and N % 5 != 0:
+#         sum += N
+#         N -= 1
+#     else:
+#         N -= 1
 
-print(sum)
+# print(sum)
+
+N = int(input())
+total = 0
+
+for i in range(1, N + 1):
+    if i % 3 != 0 and i % 5 != 0:
+        total += i
+
+print(total)
