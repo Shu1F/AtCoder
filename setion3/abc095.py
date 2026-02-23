46303ab0a566511fc8bda27aa8bb8d5e15d90bc5
Shuichi Fujiike <fs2031@icloud.com>
2026-02-23 23:00:10 +0900

    update

--- setion3/abc095.py
+++ setion3/abc095.py
@@ -2,8 +2,9 @@ N, X = map(int, input().split())
 m = list(map(int, input().split()))
 
 
-for i in range(m[i]):
+for i in range(len(m)):
     X -= m[i]
+
 min_amount = min(m)
 
 print(X//min_amount + len(m))
