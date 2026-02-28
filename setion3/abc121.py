1ce985a66a593938240d2329bde5dbb142146bc4
Shuichi Fujiike <fs2031@icloud.com>
2026-02-28 17:31:19 +0900

    update

---
+++ setion3/abc121.py
@@ -0,0 +1,19 @@
+N, M = map(input().split())
+pairs = []
+for _ in range(N):
+    A, B = map(int, input().split())
+    pairs.append((A, B))
+
+pairs.sort()
+
+bought = 0
+ans = 0
+
+for A, B in pairs:
+    if bought == ans:
+        break
+    buy = min(B, M - bought)
+        ans += A * buy
+    bought += buy
+
+print(ans)
